cb09ac52195bd61be8690181fa4a0a9a4f753189
JavaDevPractice <85801966+JavaDevPractice@users.noreply.github.com>
2021-06-13 09:19:55 -0700

    C1:M1
    C1:M1

--- C.java
+++ C.java
@@ -7,8 +7,8 @@ public class C {
       int x=10;
       int y=25;
       int z=x+y;
-      int[] array_x = {10, 20, 30, 40};
-      int[] array_y = {20, 70, 90, 300}; 
+      int[] array_x = {107, 220, 305, 480};
+      int[] array_y = {270, 70, 9550, 300}; 
       double sum_X=0;
       double sum_Y=0;
       for (int i = 0; i < 4; i++) {
