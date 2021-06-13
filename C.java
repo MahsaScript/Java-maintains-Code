4043ebea2f739d1b9699bddbd4410953da923a28
JavaDevPractice <85801966+JavaDevPractice@users.noreply.github.com>
2021-06-13 09:16:09 -0700

    File C Java

---
+++ C.java
@@ -0,0 +1,25 @@
+public class C {
+    public static void main(String args[]) {
+      C c_obj=new C();
+      c_obj.M();
+    }
+    public static void M() {
+      int x=10;
+      int y=25;
+      int z=x+y;
+      int[] array_x = {10, 20, 30, 40};
+      int[] array_y = {20, 70, 90, 300}; 
+      double sum_X=0;
+      double sum_Y=0;
+      for (int i = 0; i < 4; i++) {
+          sum_X = sum_X + array_x[i];
+          sum_Y = sum_Y + array_y[i];
+      }
+      double mean_x = sum_X/4;
+      double mean_y = sum_Y/4;
+      System.out.println("S");
+      System.out.println("The average of List X is " + mean_x);
+      System.out.println("The average of List Y is " + mean_y);
+      System.out.println("E");
+    }
+}
